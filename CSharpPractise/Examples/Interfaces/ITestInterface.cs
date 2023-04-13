90021b5a0de807b1d45549129a57ec82dba86559
rahulkkushwaha <rahulkkushwaha@hotmail.com>
2023-04-13 21:21:06 +0530

    added interfaces

---
+++ CSharpPractise/Examples/Interfaces/ITestInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractise.Examples.Interfaces
+{
+    interface ITestInterface
+    { 
+        void Add(int num1, int num2);
+    }
+}

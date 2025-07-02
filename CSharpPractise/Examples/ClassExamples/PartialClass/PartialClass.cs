c7747e015a5c53e637f3646f7c0aa964554cde5a
Raj Kushwaha <41170098+RajTheCreater@users.noreply.github.com>
2025-07-02 22:39:32 +0530

    PartialClass

--- CSharpPractise/Examples/ClassExamples/PartialClass/PartialClass.cs
+++ CSharpPractise/Examples/ClassExamples/PartialClass/PartialClass.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace CSharpPractise.Examples.ClassExamples.PartialClass
 {
-    internal class PartialClass
+    public partial class PartialEmployee
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public double Salary { get; set; }
+    }
+    public partial class PartialEmployee
+    {
+        public void DisplayFullName()
+        {
+            Console.WriteLine($"Full Name is : {FirstName} {LastName}");
+        }
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine("Employee Details : ");
+            Console.WriteLine($"First Name : {FirstName}");
+            Console.WriteLine($"Last Name : {LastName}");
+            Console.WriteLine($"Gender : {Gender}");
+            Console.WriteLine($"Salary : {Salary}");
+        }
+    }
+    public class PartialClass
+    {
+        public static void Main()
+        {
+            PartialEmployee employee = new PartialEmployee();
+            employee.
+        }
     }
 }
